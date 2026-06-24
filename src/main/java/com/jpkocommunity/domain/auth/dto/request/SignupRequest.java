7b0a5f3805623fc899345a50fb52c801a78c0160
ahdeka <ahdeka01@gmail.com>
2026-06-24 16:57:40 +0900

    refactor: 비밀번호 상한 50자로 변경

--- src/main/java/com/jpkocommunity/domain/auth/dto/request/SignupRequest.java
+++ src/main/java/com/jpkocommunity/domain/auth/dto/request/SignupRequest.java
@@ -11,7 +11,7 @@ public record SignupRequest(
         String email,
 
         @NotBlank(message = "닉네임을 입력해주세요.")
-        @Size(min = 2, max = 20, message = "닉네임은 2자 이상 20자 이하여야 합니다.")
+        @Size(min = 2, max = 50, message = "닉네임은 2자 이상 50자 이하여야 합니다.")
         String nickname,
 
         @NotBlank(message = "비밀번호를 입력해주세요.")
--- src/main/java/com/jpkocommunity/domain/user/dto/request/UpdatePasswordRequest.java
+++ src/main/java/com/jpkocommunity/domain/user/dto/request/UpdatePasswordRequest.java
@@ -8,7 +8,7 @@ public record UpdatePasswordRequest(
         String currentPassword,
 
         @NotBlank(message = "새 비밀번호를 입력해주세요.")
-        @Size(min = 8, max = 20, message = "비밀번호는 8~20자 사이여야 합니다.")
+        @Size(min = 8, max = 50, message = "비밀번호는 8~50자 사이여야 합니다.")
         String newPassword,
 
         @NotBlank(message = "새 비밀번호 확인을 입력해주세요.")
