64e2b8df5ced9a7720f345e5ce082dad7434dc01
ahdeka <ahdeka01@gmail.com>
2026-06-24 17:03:57 +0900

    fix: 비밀번호 50자 상한 수정

--- src/main/java/com/jpkocommunity/domain/auth/dto/request/SignupRequest.java
+++ src/main/java/com/jpkocommunity/domain/auth/dto/request/SignupRequest.java
@@ -11,11 +11,11 @@ public record SignupRequest(
         String email,
 
         @NotBlank(message = "닉네임을 입력해주세요.")
-        @Size(min = 2, max = 50, message = "닉네임은 2자 이상 50자 이하여야 합니다.")
+        @Size(min = 2, max = 20, message = "닉네임은 2자 이상 20자 이하여야 합니다.")
         String nickname,
 
         @NotBlank(message = "비밀번호를 입력해주세요.")
-        @Size(min = 8, max = 20, message = "비밀번호는 8~20자 사이여야 합니다.")
+        @Size(min = 8, max = 50, message = "비밀번호는 8~50자 사이여야 합니다.")
         String password,
 
         @NotBlank(message = "비밀번호 확인을 입력해주세요.")
